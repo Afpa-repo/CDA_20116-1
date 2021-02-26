c452f9d86a604d71e4572096c7c5bb25288d39d0
juliencdp <jul.pichon@gmail.com>
2021-02-26 13:16:05 +0100

    V.0.5 paiement

---
+++ src/Controller/OrderCancelController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderCancelController extends AbstractController
+{
+
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager; // Instanciation d'entityManagerInterface -- Pour mettre les modifications en BDD
+    }
+
+    /**
+     * @Route("/commande/erreur/{stripeSessionId}", name="order_cancel")
+     */
+    public function index($stripeSessionId): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
+
+        //Si order n'est pas trouvé ou si l'order est différent de l'utilisateur connecté, on redirige sur home
+        if(!$order || $order->getUser() != $this->getUser() )
+        {
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('order_cancel/index.html.twig', [
+            'order' => $order
+        ]);
+    }
+}
